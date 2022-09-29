a3f6a8f7103faa493246d51f6b5d5c0eb1933c27
arist76 <https://github.com/arist76>
2022-09-29 21:31:07 +0300

    delete debugging log

--- MaximumNumberOfCoins.js
+++ MaximumNumberOfCoins.js
@@ -3,7 +3,6 @@ var maxCoins = function(piles) {
     let sum = 0
     let pile_length = sorted_piles.length/3
     for (let i = 0; i < pile_length; i++) {
-        console.log(i)
         sorted_piles.pop()
         sum += sorted_piles.pop()
         sorted_piles.splice(0,1)
